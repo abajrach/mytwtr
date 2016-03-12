b9905ce2723794537b64a2d40712b8ee141e39ab
Arbindra Bajracharya <arbindrabajracharya@gmail.com>
2016-03-12 14:34:23 -0600

    Added comments

--- grails-app/controllers/mytwtr/AccountController.groovy
+++ grails-app/controllers/mytwtr/AccountController.groovy
@@ -6,7 +6,7 @@ import grails.rest.RestfulController
 import java.text.SimpleDateFormat
 
 
-class AccountController extends RestfulController<Account>{
+class AccountController extends RestfulController<Account> {
     static responseFormats = ['json', 'xml']
 
     AccountController() {
@@ -14,6 +14,7 @@ class AccountController extends RestfulController<Account>{
     }
 
     /* This returns appropriate error message if specified account is not found */
+
     def show() {
 
         def account = Account.get(params.id)
@@ -25,20 +26,19 @@ class AccountController extends RestfulController<Account>{
     }
 
     /* Called when GET on /accounts URI without Ids */
+
     def index() {
 
-        if(params.handle) {
+        if (params.handle) {
 
             def account = Account.findByHandlename(params.handle)
-            if(account) {
+            if (account) {
                 render account as JSON
-            }
-            else {
+            } else {
                 response.status = 404 // @Todo: Fix the notFound.gsp issue. Return status is 404 but the page is empty
-                                      // @Todo: Set error status message
+                // @Todo: Set error status message
             }
-        }
-        else {
+        } else {
             // Return all the accounts
             render Account.getAll() as JSON
         }
@@ -67,44 +67,53 @@ class AccountController extends RestfulController<Account>{
     }
 
     /*
-    @Todo: Figure out if we need to get rid of Followers and FollowedBy from the output
+        Returns the list of followers for an account
+        max and offset query parameters can be used to filter the maximum and offset value for the result
      */
+
     def getFollowers() {
 
         def accountId = Integer.parseInt(params.id)
-        def max       = params.max ? Integer.parseInt(params.max) : 10
-        def offset    = params.offset ? Integer.parseInt(params.offset) : 0
+        def max = params.max ? Integer.parseInt(params.max) : 10
+        def offset = params.offset ? Integer.parseInt(params.offset) : 0
 
-        if(Account.get(accountId)) {
+        if (Account.get(accountId)) {
             def followers = Account.findAll('from Account acc where acc.id in (:accounts)',
                     [accounts: Account.get(accountId).followedBy.id],
                     [max: max, offset: offset, order: 'asc'])
 
             render followers as JSON
-        }
-        else {
+        } else {
             render(status: 404, text: "Specified account with account ID ${accountId} doesn't exists")
         }
     }
 
-    def shownewsfeed() {
-        def accountId   = Integer.parseInt(params.id)
-        def limit       = params.limit ? Integer.parseInt(params.limit) : 10
-        def from        = params.from
+    /*
+        Shows the messages of the accounts that are being followed.
+        The messages are displayed in descending order of date created
+        By default, only 10 messages are displayed. This value can be changed by using ?limit= <max> query
+        http://localhost:8080/accounts/1/shownewsfeed?limit=5
+        ?from=<date> query can be used to filter messages that are created after that date
+        Example: http://localhost:8080/accounts/1/shownewsfeed?from=2016-03-12T20:21:31Z
+     */
 
-        if(Account.get(accountId)) {
+    def shownewsfeed() {
+        def accountId = Integer.parseInt(params.id)
+        def limit = params.limit ? Integer.parseInt(params.limit) : 10
+        def from = params.from
+
+        if (Account.get(accountId)) {
             def messageResults = Message.withCriteria {
-               'in' ('account', Account.get(accountId).following)
+                'in'('account', Account.get(accountId).following)
                 maxResults(limit)
                 order('dateCreated', 'desc')
-                if(from) {
+                if (from) {
                     gte('dateCreated', new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX").parse(from))
                 }
             }
 
             render messageResults as JSON
-        }
-        else {
+        } else {
             render(status: 404, text: "Specified account with account ID ${accountId} doesn't exists")
         }
 
