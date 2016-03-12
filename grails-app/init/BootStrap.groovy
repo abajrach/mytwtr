7707d42e4274067101c723eec756266520d3b241
Arbindra Bajracharya <arbindrabajracharya@gmail.com>
2016-03-12 14:17:06 -0600

    Fixed status issues and removed code from Boostrap.groovy

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -4,37 +4,6 @@ import mytwtr.Message
 class BootStrap {
 
     def init = { servletContext ->
-/*
-        def foobar = new Account(handlename: 'foobar', name: 'Mr. Foo Bar', password: 'pSDsfsfsFD2', email: 'foo_bar@gmail.com').save()
-        def darthVader = new Account(handlename: 'darthVader', name: 'Mr. Darth Vader', password: 'd8RTHV8der1', email: 'darth_vader@gmail.com').save()
-        def darthVader2 = new Account(handlename: 'darthVader2', name: 'Mr. Darth Vader', password: 'd8RTHV8der1', email: 'darth_vader2@gmail.com').save()
-        def darthVader3 = new Account(handlename: 'darthVader3', name: 'Mr. Darth Vader', password: 'd8RTHV8der1', email: 'darth_vader3@gmail.com').save()
-        def darthVader4 = new Account(handlename: 'darthVader4', name: 'Mr. Darth Vader', password: 'd8RTHV8der1', email: 'darth_vader4@gmail.com').save()
-
-        def kkardashian = new Account(handlename: 'kkardashian', name: 'Kim Kardashian', password: 'kimkdshAn1', email: 'kim_kardashaian@gmail.com').save()
-
-        // both foobar and darthvader following kkardashian
-        foobar.addToFollowing(kkardashian).save(flush: true, failOnError: true)
-        kkardashian.addToFollowedBy(foobar).save(flush: true, failOnError: true)
-
-        darthVader.addToFollowing(kkardashian).save(flush: true, failOnError: true)
-        kkardashian.addToFollowedBy(darthVader).save(flush: true, failOnError: true)
-
-        darthVader2.addToFollowing(kkardashian).save(flush: true, failOnError: true)
-        kkardashian.addToFollowedBy(darthVader2).save(flush: true, failOnError: true)
-
-        darthVader3.addToFollowing(kkardashian).save(flush: true, failOnError: true)
-        kkardashian.addToFollowedBy(darthVader3).save(flush: true, failOnError: true)
-
-        darthVader4.addToFollowing(kkardashian).save(flush: true, failOnError: true)
-        kkardashian.addToFollowedBy(darthVader4).save(flush: true, failOnError: true)
-
-        def message1 = new Message(status_message: "emJaysfa ", account: kkardashian).save(flush: true, failOnError: true)
-        def message2 = new Message(status_message: "lisaAnn afd asd", account: kkardashian).save(flush: true, failOnError: true)
-        def message3 = new Message(status_message: "playboy lisaAnn magazine", account: kkardashian).save(flush: true, failOnError: true)
-        def message4 = new Message(status_message: "playboy magazine lisaAnn", account: kkardashian).save(flush: true, failOnError: true)
-        def message5 = new Message(status_message: "playboy magazine lisaAnn2", account: kkardashian).save(flush: true, failOnError: true)
-        */
     }
     def destroy = {
     }
