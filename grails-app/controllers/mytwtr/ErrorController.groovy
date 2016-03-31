edebefc4011dafdd06fafc9ff80bf00e36b13fb8
Arbindra Bajracharya <arbindrabajracharya@gmail.com>
2016-03-30 23:01:00 -0400

    Fixed ErrorController issue

--- grails-app/controllers/mytwtr/ErrorController.groovy
+++ grails-app/controllers/mytwtr/ErrorController.groovy
@@ -6,37 +6,21 @@ class ErrorController {
 
     def internalServerError() {
         response.status = 500
-/*        render(contentType: 'application/json') {
-            error = response.status
-            message = 'Internal server error'
-        }*/
         render ([error: response.status, message: "Internal server error"] as JSON)
     }
 
     def notFound() {
-/*        response.status = 404
-        render(contentType: 'application/json') {
-            error = response.status
-            message = 'Not found'
-        }*/
+        response.status = 404
         render ([error: response.status, message: "Not foun"] as JSON)
     }
 
     def unauthorized() {
         response.status = 401
-/*        render(contentType: 'application/json') {
-            error = response.status
-            message = 'Unauthorized'
-        }*/
         render ([error: response.status, message: "Unauthorized"] as JSON)
     }
 
     def forbidden() {
         response.status = 403
-/*        render(contentType: 'application/json') {
-            error = response.status
-            message = 'Forbidden'
-        }*/
         render ([error: response.status, message: "Forbidden"] as JSON)
     }
 }
