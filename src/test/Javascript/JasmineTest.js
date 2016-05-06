e9c60a819cdad4372a50f31beb0f3b3792704c6d
shopopie <shopopie@yahoo.com>
2016-05-06 15:28:02 -0500

    Merge branch 'assignment3' of https://github.com/abajrach/mytwtr into assignment3
    # Conflicts:
    #	src/integration-test/groovy/mytwtr/uitests/SearchRequirementTests.groovy

--- src/test/Javascript/JasmineTest.js
+++ src/test/Javascript/JasmineTest.js
@@ -1,4 +1,4 @@
-describe("true", function()
+/*describe("true", function()
 {
     it("Should be true", function(){
         expect(true).toBeTruthy()
@@ -8,91 +8,41 @@ describe("true", function()
 });
 
 
-
-
-/*
-
-
-
- describe('MessageController', function () {
-
- var $controller, $httpBackend, $scope;
- var now = new Date();
- var posts = [
- {song: {title: 'Loser', artist: {name: 'Beck'}}, timestamp: now},
- {song: {title: 'Enjoy The Silence', artist: {name: 'Depeche Mode'}}, timestamp: now}];
-
- beforeEach(module('app'));
-
- beforeEach(inject(function ($injector) {
- $controller = $injector.get('$controller');
- $httpBackend = $injector.get('$httpBackend');
- $scope = $injector.get('$rootScope').$new();
- }));
-
- describe('initializes scope', function () {
-
- it('fetches the play data into scope', function () {
- $httpBackend.expectGET('play/').respond(200, plays);
- $controller('PostMessageController', {$scope: $scope});
- $httpBackend.flush();
-
- expect($scope.plays).toEqual(plays);
- });
-
- });
-
- afterEach(function () {
- $httpBackend.verifyNoOutstandingExpectation();
- $httpBackend.verifyNoOutstandingRequest();
- });
- });
-
-
-
- */
-
-
-
-
-
-
-
+*/
 
 describe('messageService', function () {
     beforeEach(module('app'));
 
-
     var messageService;
     var $httpBackend;
-
 
     beforeEach(inject(function (_messageService_, _$httpBackend_) {
         messageService = _messageService_;
         $httpBackend = _$httpBackend_;
+        console.log('------------------0---------------------')
     }));
 
 
     describe('save', function () {
         it('handles save message', function () {
-            $httpBackend.expectPOST('/accounts/1/messages', {selfId: 1},  payLoad).respond(200, {username: 'earl', roles: ['root'], 'access_token': 'xyz123'});
-            messageService.save('u', 'p');
+        //    $httpBackend.expectPOST('/accounts/1/messages', {status_message:'test', account:'1'}).respond(201, {messageId: '1', status_message: 'test'});
+            console.log('------------------1---------------------')
+            $httpBackend.expectPOST('/accounts/messages', {status_message:'test',id:1}).respond(201, {id:1,status_message:'test'});
+            console.log('------------------2---------------------')
+           // messageService.postMessage(1, 'test');
+            messageService.$resource.post(1, 'test');
+            console.log('------------------3---------------------')
             $httpBackend.flush();
-            var user = messageService.currentUser();
-            //   expect(user).toBeDefined();
-            //   expect(user.username).toBe('earl');
-            //   expect(user.roles).toContain('root');
-            //   expect(user.token).toBe('xyz123');
+            console.log('------------------4---------------------')
+            var message = messageService.status_message();
+            console.log('------------------5---------------------')
+            expect(message).toBeDefined();
+            console.log('------------------6---------------------')
+            console.log("Message -> ", message)
+            expect(message = 'test');
         });
 
 
-        //  it('handles invalid message too long', function () {
-        //      $httpBackend.expectPOST('/api/login', {username: 'u', password: 'p'}).respond(403, {status: 403, message: 'Forbidden'});
-        //      securityService.login('u', 'p');
-        //      $httpBackend.flush();
-        //      var user = securityService.currentUser();
-        //      expect(user).toBeUndefined();
-        //  });
     });
 
 
