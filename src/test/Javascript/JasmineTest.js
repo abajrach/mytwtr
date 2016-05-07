5cbedcc3d367f237640cdb9c465921ca364c43b8
shopopie <shopopie@yahoo.com>
2016-05-06 22:37:44 -0500

    JasmineTest run commit

--- src/test/Javascript/JasmineTest.js
+++ src/test/Javascript/JasmineTest.js
@@ -1,55 +1,47 @@
-/*describe("true", function()
- {
- it("Should be true", function(){
- expect(true).toBeTruthy()
-
- });
-
- });
-
-
- */
-
 describe('messageService', function () {
     beforeEach(module('app'));
 
     var messageService;
+    var $controller;
     var $httpBackend;
+    var $scope;
 
-    beforeEach(inject(function (_messageService_, _$httpBackend_) {
-        messageService = _messageService_;
-        $httpBackend = _$httpBackend_;
-        console.log('------------------0---------------------')
+    beforeEach(inject(function ($injector) {
+        $controller = $injector.get('$controller');
+        $httpBackend = $injector.get('$httpBackend');
+        $scope = $injector.get('$rootScope').$new();
+        messageService = $injector.get('messageService');
     }));
 
-
     describe('save', function () {
-        it('handles save message', function () {
-            //    $httpBackend.expectPOST('/accounts/1/messages', {status_message:'test', account:'1'}).respond(201, {messageId: '1', status_message: 'test'});
-            console.log('------------------1---------------------')
-            $httpBackend.expectPOST('/accounts/messages', {status_message:'test',id:1}).respond(201, {id:1,status_message:'test'});
-            console.log('------------------2---------------------')
-            // messageService.postMessage(1, 'test');
-            messageService.$resource.post(1, 'test');
-            console.log('------------------3---------------------')
+        it('handles post message', function () {
+            $httpBackend.expectPOST('/accounts/1/messages', {"status_message":"test","account":1}).respond(201, {"status_message":"test","account":1});
+
+            var payLoad = {"status_message": "test", "account": 1};
+            var returnMessage = messageService.save({selfId: 1}, payLoad);
+            console.log(returnMessage);
             $httpBackend.flush();
-            console.log('------------------4---------------------')
-            var message = messageService.status_message();
-            console.log('------------------5---------------------')
-            expect(message).toBeDefined();
-            console.log('------------------6---------------------')
-            console.log("Message -> ", message)
-            expect(message = 'test');
+
+            expect(returnMessage.status_message == 'test');
+            expect(returnMessage.account == 1);
+
+
         });
 
+        it('Spy on messageService', function(){
+            spyOn(messageService, 'save');
+            var payLoad = {"status_message": "test", "account": 1};
+            var returnMessage = messageService.save({selfId: 1}, payLoad);
+            console.log(returnMessage);
+
+            expect(messageService.save).toHaveBeenCalled();
+        });
 
     });
-
 
     afterEach(function () {
         $httpBackend.verifyNoOutstandingExpectation();
         $httpBackend.verifyNoOutstandingRequest();
     });
-
 
 });
